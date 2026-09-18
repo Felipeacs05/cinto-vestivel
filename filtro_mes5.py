53a3ca4ec370f6a34730dbb0ba670dffb6f510a1
Felipeacs05 <felipedocarmo05@gmail.com>
2026-09-18 10:42:54 -0300

    # 1. CARREGAMENTO DOS DADOS (Caminhada do Idoso)

--- filtro_mes5.py
+++ filtro_mes5.py
@@ -2,3 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import butter, filtfilt
+
+caminho_arquivo = 'SisFall_dataset/SE01/D01_SE01_R01.txt'
+nomes_colunas = ['ADXL_X', 'ADXL_Y', 'ADXL_Z', 'ITG_X', 'ITG_Y', 'ITG_Z', 'MMA_X', 'MMA_Y', 'MMA_Z']
+df = pd.read_csv(caminho_arquivo, sep=',', names=nomes_colunas, engine='python')
+df['MMA_Z'] = df['MMA_Z'].astype(str).str.replace(';', '').astype(float)
