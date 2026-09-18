f6220d4bf93e5473f9e413169d7d1ca0e72dc5e1
Felipeacs05 <felipedocarmo05@gmail.com>
2026-09-18 10:43:17 -0300

    conversão para g

--- filtro_mes5.py
+++ filtro_mes5.py
@@ -7,3 +7,7 @@ caminho_arquivo = 'SisFall_dataset/SE01/D01_SE01_R01.txt'
 nomes_colunas = ['ADXL_X', 'ADXL_Y', 'ADXL_Z', 'ITG_X', 'ITG_Y', 'ITG_Z', 'MMA_X', 'MMA_Y', 'MMA_Z']
 df = pd.read_csv(caminho_arquivo, sep=',', names=nomes_colunas, engine='python')
 df['MMA_Z'] = df['MMA_Z'].astype(str).str.replace(';', '').astype(float)
+
+df['Eixo_X_g'] = df['ADXL_X'] / 256.0
+df['Eixo_Y_g'] = df['ADXL_Y'] / 256.0
+df['Eixo_Z_g'] = df['ADXL_Z'] / 256.0
