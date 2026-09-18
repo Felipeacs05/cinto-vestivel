4859d85ab4a7ee1f678a3a6cc8ac564c8be78f05
Felipeacs05 <felipedocarmo05@gmail.com>
2026-09-18 10:47:36 -0300

    Plotagem do comparativo

--- filtro_mes5.py
+++ filtro_mes5.py
@@ -29,3 +29,5 @@ df['Z_filtrado'] = filtfilt(b, a, df['Eixo_Z_g'])
 #Um Bruto e um Filtrado para comparação
 df['SVM_Bruto'] = np.sqrt(df['Eixo_X_g']**2 + df['Eixo_Y_g']**2 + df['Eixo_Z_g']**2)
 df['SVM_Filtrado'] = np.sqrt(df['X_filtrado']**2 + df['Y_filtrado']**2 + df['Z_filtrado']**2)
+
+tempo_segundos = np.arange(len(df)) / frequencia_amostragem
